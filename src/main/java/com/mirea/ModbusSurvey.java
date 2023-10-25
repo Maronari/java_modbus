d98cabf7713da1ae1d602866956ca8425d540278
Ivan Korsunov <ivan.corsunov@gmail.com>
2023-10-25 22:10:16 +0300

    modbus broadcast

--- src/main/java/com/mirea/ModbusSurvey.java
+++ src/main/java/com/mirea/ModbusSurvey.java
@@ -10,9 +10,13 @@ import com.intelligt.modbus.jlibmodbus.exception.ModbusNumberException;
 import com.intelligt.modbus.jlibmodbus.exception.ModbusProtocolException;
 import com.intelligt.modbus.jlibmodbus.master.ModbusMaster;
 import com.intelligt.modbus.jlibmodbus.master.ModbusMasterFactory;
+import com.intelligt.modbus.jlibmodbus.serial.SerialParameters;
+import com.intelligt.modbus.jlibmodbus.serial.SerialPort;
 import com.intelligt.modbus.jlibmodbus.tcp.TcpParameters;
 
-public class App {
+import jssc.SerialPortList;
+
+public class ModbusSurvey {
 
     public static int convertToSigned16Bit(int unsignedValue) {
         if ((unsignedValue & (1 << 15)) != 0) { // if the sign bit is set, the number is negative
@@ -24,44 +28,67 @@ public class App {
 
     public static void main(String[] args) {
         try {
+            String[] dev_list = SerialPortList.getPortNames();
+            //System.out.println("COM's: ");
+            for (String string : dev_list) {
+                System.out.print(string + " ");
+            }
+
             TcpParameters tcpParameters = new TcpParameters();
+            SerialParameters serialParameters = new SerialParameters();
+
+            // serial: COM1, 9600, 8
+            // serialParameters.setDevice("COM2");
+            serialParameters.setBaudRate(SerialPort.BaudRate.BAUD_RATE_9600);
+            serialParameters.setDataBits(8);
+            serialParameters.setParity(SerialPort.Parity.NONE);
+            serialParameters.setStopBits(1);
 
             // tcp: 127.0.0.1:502
             tcpParameters.setHost(InetAddress.getLocalHost());
             tcpParameters.setKeepAlive(true);
             tcpParameters.setPort(Modbus.TCP_PORT);
 
-            ModbusMaster m = ModbusMasterFactory.createModbusMasterTCP(tcpParameters);
+            ModbusMaster master = ModbusMasterFactory.createModbusMasterTCP(tcpParameters);
             Modbus.setAutoIncrementTransactionId(true);
+            master.setResponseTimeout(100);
 
-            int slaveId = 1;
-            int quantity = 16;
+            // ModbusMaster master =
+            // ModbusMasterFactory.createModbusMasterRTU(serialParameters);
+            // master.connect();
 
+            int slaveId = 0;
+            int quantity = 2;
             try {
-                if (!m.isConnected()) {
-                    m.connect();
+                if (!master.isConnected()) {
+                    master.connect();
                 }
 
                 SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+                Date date = new Date(System.currentTimeMillis());
+                System.out.println(formatter.format(date));
 
-                // at next string we receive ten registers from a slave with id of 1 at offset
-                // of 0.
                 while (true) {
-
-                    int offset = 0;
-                    int[] registerValues = m.readInputRegisters(slaveId, offset, quantity);
-                    Date date = new Date(System.currentTimeMillis());
-                    System.out.println(formatter.format(date));
-
-                    for (int i = 1; i < registerValues.length; i += 2) {
-                        int value = registerValues[i];
-                        System.out.println("Address: " + offset + ", Value: " + (convertToSigned16Bit(value)));
-                        offset += 2;
+                    while (slaveId < 255) {
+                        try {
+                            slaveId++;
+                            int offset = 0;
+                            int[] registerValues = master.readInputRegisters(slaveId, offset, quantity);
+                            for (int i = 1; i < registerValues.length; i += 2) {
+                                // int value = registerValues[i];
+                                System.out.println("Slave: " + slaveId);
+                                offset += 2;
+                            }
+                        } catch (ModbusIOException e) {
+                            continue;
+                        }
                     }
-
+                    slaveId = 0;
                     System.out.println("--------------------------------------------------------");
                     Thread.sleep(2000);
+
                 }
+
             } catch (ModbusProtocolException e) {
                 e.printStackTrace();
             } catch (ModbusNumberException e) {
@@ -70,7 +97,7 @@ public class App {
                 e.printStackTrace();
             } finally {
                 try {
-                    m.disconnect();
+                    master.disconnect();
                 } catch (ModbusIOException e) {
                     e.printStackTrace();
                 }
